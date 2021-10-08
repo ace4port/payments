21803e34b885b03f6efb1db96cd85a7deb457b53
zenith rajbhandari <zenithrajbhandari@gmail.com>
2021-10-08 12:56:52 +0545

    'styles'

--- src/container/Payment/InvoicePay.jsx
+++ src/container/Payment/InvoicePay.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import Button from '../../components/Button'
-
+import PrintIcon from '../../components/icons/PrintIcon'
 const InvoicePay = ({ history }) => {
   return (
     <div>
       <h1>Invoices</h1>
       <h2>Pay your invoices</h2>
+      <div className="invoice-container">
+        <div className="invoice-container__details">
+          <h5 className="invoiceState">Pending</h5>
 
-      <h5>Pending</h5>
-
-      <p>invoice #122552</p>
-      <p>Total: $420</p>
-
-      <Button
-        size="small"
-        variant="outlined"
-        onClick={() => console.log('Print')}
-      >
-        Print
-      </Button>
-      <Button size="medium" onClick={() => history.push('/payments')}>
-        Pay now
-      </Button>
+          <p className="invoice">
+            Invoice <span className="invoice-id">#122552</span>
+          </p>
+          <hr />
+          <p className="total">
+            Total: <span className="total-amount">$420</span>
+          </p>
+        </div>
+        <div className="invoice-container__button">
+          <div
+            className="print-icon"
+            onClick={() => {
+              console.log('Print')
+            }}
+          >
+            <PrintIcon></PrintIcon>
+          </div>
+          <Button size="medium" onClick={() => history.push('/payments')}>
+            Pay now
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
