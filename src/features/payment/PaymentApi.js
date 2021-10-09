e9392c699dc7e3de6d74ebac05fe298fa94e6708
ace4port <39233007+ace4port@users.noreply.github.com>
2021-10-09 21:35:22 +0545

    https

--- src/features/payment/PaymentApi.js
+++ src/features/payment/PaymentApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let url = 'http://e706-49-244-15-6.ngrok.io/api'
+let url = 'https://e706-49-244-15-6.ngrok.io/api'
 
 export const getInvoices = async () => await axios.get(`${url}/invoice/`)
 
